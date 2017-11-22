845d6fe20ae1b465e69d9de61bd7737dc8dcfd63
IovoslavIovchev <iovoslav.i@gmail.com>
2017-11-22 12:15:52 +0200

    Initial Commit

---
+++ 00. Entrance_Exam - 13Oct2017/03. Hops/Hops.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Hops
+{
+    static void Main(string[] args)
+    {
+        //Hops by IovoslavIovchev
+        //Gives only 95/100 points!!!
+        int[] carrots = Console.ReadLine()
+            .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        short m = short.Parse(Console.ReadLine()); int max = int.MinValue; //directions to try
+        for (short i = 0; i < m; i++)
+        {
+            List<int> visited = new List<int>();
+            int[] directions = Console.ReadLine()
+                .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int position = 0, temp = 0, x = 0;
+            while (!visited.Contains(position))
+            {
+                if (position < 0 || position >= carrots.Length)
+                    break;
+
+                temp += carrots[position];
+                visited.Add(position);
+
+                position += directions[x++];
+
+                if (x == directions.Length)
+                    x = 0;
+            }
+            if (max < temp)
+                max = temp;
+        }
+
+        Console.WriteLine(max);
+    }
+}
+
