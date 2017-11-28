43751df951144e305d65c3d53c0e6b58240e9bdb
IovoslavIovchev <iovoslav.i@gmail.com>
2017-11-28 20:02:33 +0200

    Added LargestAreaInMatrix

--- 01. Multidimentional_Arrays/06. Largest_Area_in_Matrix/Program.cs
+++ 01. Multidimentional_Arrays/06. Largest_Area_in_Matrix/Program.cs
@@ -1,26 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Largest_Area_in_Matrix
 {
     class Program
     {
+        static int r;
+        static int c;
+        static int[][] matrix;
+        static bool[,] visited;
+        static int max;
+        static int temp;
+
         static void Main(string[] args)
         {
-            int[] size = Console.ReadLine()
-                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToArray();
-            int x = size[0], y = size[1];
+            int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            r = size[0]; c = size[1];
 
-            short[][] matrix = new short[x][];
-            for (int i = 0; i < x; i++)
-            {
+            matrix = new int[r][];
+            visited = new bool[r, c];
+
+            for (int i = 0; i < r; i++)
                 matrix[i] = Console.ReadLine()
-                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                    .Select(short.Parse)
+                    .Split()
+                    .Select(int.Parse)
                     .ToArray();
+            
+
+            for (int i = 0; i < r; i++) //i for rows
+            {
+                for (int j = 0; j < c; j++) //j for cols
+                {
+                    if (!visited[i, j])
+                    {
+                        temp = 0;
+                        DFS(i, j, matrix[i][j], matrix);
+                    }
+                }
             }
+
+            Console.WriteLine(max);
+        }
+
+        static void DFS(int i, int j, int num, int[][] m)
+        {
+            if (i < 0 || i > r - 1 || j < 0 || j > c - 1)
+                return;
+            if (m[i][j] != num)
+                return;
+            if (visited[i, j])
+                return;
+
+            temp++;
+            visited[i, j] = true;
+            max = Math.Max(max, temp);
+
+            DFS(i - 1, j, num, m);
+            DFS(i + 1, j, num, m);
+            DFS(i, j - 1, num, m);
+            DFS(i, j + 1, num, m);            
         }
     }
 }
--- 03. Methods/08. Number_as_Array/Program.cs
+++ 03. Methods/08. Number_as_Array/Program.cs
@@ -7,7 +7,47 @@ namespace Number_as_Array
     {
         static void Main(string[] args)
         {
+            int max = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray().Max();
+
+            int[] first = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] second = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] result = new int[max + 1];
             
+            AddToArray(result, first);
+            AddToArray(result, second);
+
+            if (result[result.Length-1] == 0)
+                Console.WriteLine(string.Join(" ", result.Take(result.Length - 1)));
+            else
+                Console.WriteLine(string.Join(" ", result));
+        }
+
+        static void AddToArray(int[] result, int[] array)
+        {
+            int rem = 0;
+            for (int i = 0; i < array.Length; i++)
+            {                
+                result[i] += array[i] + rem;
+
+                if (result[i] >= 10)
+                {
+                    rem = 1;
+                    result[i] -= 10;
+                }            
+                else
+                    rem = 0;
+            }
+
+            result[result.Length - 1] += rem;
         }
     }
 }
