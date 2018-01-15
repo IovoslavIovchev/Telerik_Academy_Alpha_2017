d4eefd54e75ec772224d9f6b790527340f95405c
IovoslavIovchev <iovoslav.i@gmail.com>
2018-01-15 23:39:24 +0200

    Added Searching and Sorting Algorithms

--- 03. Data_Structures_and_Algorithms/04. Searching_and_Sorting_Algorithms/02. Sorting_Algorithms/SortableCollection.cs
+++ 03. Data_Structures_and_Algorithms/04. Searching_and_Sorting_Algorithms/02. Sorting_Algorithms/SortableCollection.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class SortableCollection<T> where T : IComparable<T>
     {
@@ -25,6 +26,16 @@
             }
         }
 
+        public void Add(T item)
+        {
+            this.items.Add(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            this.items.Insert(index, item);
+        }
+
         public void Sort(ISorter<T> sorter)
         {
             this.items = sorter.Sort(this.items);
@@ -45,12 +56,54 @@
 
         public bool BinarySearch(T item)
         {
-            throw new NotImplementedException();
+            if (!this.items[0].Equals(this.items[1]))
+            {
+                Sort(new SelectionSorter<T>());
+            }
+
+            return Search(item, items.ToArray(), 0, items.Count);
+        }
+
+        private bool Search(T item, T[] array, int start, int end)
+        {
+            if ((end - start <= 1 && !array[start].Equals(item) && !array[end].Equals(item)) ||
+                item.CompareTo(array[0]) < 0 || item.CompareTo(array[array.Length - 1]) > 0)
+            {
+                return false;
+            }
+
+            int mid = (start + end) / 2;
+
+            if (item.Equals(array[mid]))
+            {
+                return true;
+            }
+            else if (item.CompareTo(array[mid]) < 0)
+            {
+                return Search(item, array, start, mid);
+            }
+            else
+            {
+                return Search(item, array, mid + 1, end);
+            }
         }
 
         public void Shuffle()
         {
-            throw new NotImplementedException();
+            //using the Fisher-Yates Shuffle Algorithm
+
+            T[] array = this.items.ToArray();
+            Random random = new Random();
+            for (int i = array.Length; i > 0; i--)
+            {
+                int j = random.Next(i);
+
+                T tmp = array[j];
+                array[j] = array[i - 1];
+                array[i - 1] = tmp;
+            }
+
+            this.items = new List<T>(array);
         }
 
         public void PrintAllItemsOnConsole()
