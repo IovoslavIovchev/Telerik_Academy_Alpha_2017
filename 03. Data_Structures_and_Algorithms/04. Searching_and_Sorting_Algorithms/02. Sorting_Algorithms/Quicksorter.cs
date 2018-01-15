3856b6d5524b05e7f18f727b678e1c58b322e886
IovoslavIovchev <iovoslav.i@gmail.com>
2018-01-15 22:45:51 +0200

    Added QuickSort

--- 03. Data_Structures_and_Algorithms/04. Searching_and_Sorting_Algorithms/02. Sorting_Algorithms/Quicksorter.cs
+++ 03. Data_Structures_and_Algorithms/04. Searching_and_Sorting_Algorithms/02. Sorting_Algorithms/Quicksorter.cs
@@ -10,7 +10,61 @@
     {
         public void Sort(IList<T> collection)
         {
-            throw new NotImplementedException();
+            T[] array = collection.ToArray();
+
+            Sort(array, 0, array.Length - 1);
+
+            collection = array.ToList();
+        }
+
+        public void Sort(T[] array, int low, int high)
+        {
+            if (high <= low) return;
+
+            while (low < high)
+            {
+                int j = Partition(array, low, high);
+
+                if (j - low < high - j)
+                {
+                    Sort(array, low, j - 1);
+                    low = j + 1;
+                }
+                else
+                {
+                    Sort(array, j + 1, high);
+                    high = j - 1;
+                }
+            }
+        }
+
+        public static int Partition(T[] array, int low, int high)
+        {
+            int i = low;
+            int j = high + 1;
+
+            T pivot = array[low];
+
+            while (true)
+            {
+                while (array[++i].CompareTo(pivot) < 0) if (i == high) break;
+
+                while (pivot.CompareTo(array[--j]) < 0) if (j == low) break;
+
+                if (j <= i) break;
+
+                Swap(array, i, j);
+            }
+
+            Swap(array, low, j);
+            return j;
+        }
+
+        private static void Swap(T[] array, int i, int j)
+        {
+            T temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
         }
     }
 }
