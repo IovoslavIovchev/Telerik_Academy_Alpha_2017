58a181ba87461df3e4b96992019710eb80a6fb2a
IovoslavIovchev <iovoslav.i@gmail.com>
2018-02-04 22:34:38 +0200

    00. Workshops: Furniture: [Added an IO test]

---
+++ 04. HQC_and_DP/00. Workshops/Furniture/Task/FurnitureManufacturer.Tests/IOTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using FurnitureManufacturer.Engine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FurnitureManufacturer.Tests
+{
+    [TestClass]
+    public class IOTests
+    {
+        [TestMethod]
+        public void RunIOTest()
+        {
+            StreamReader input = new StreamReader($"./../../IOTests/001.in.txt");
+            StreamReader output = new StreamReader($"./../../IOTests/001.out.txt");
+            StringWriter result = new StringWriter();
+
+            Console.SetIn(input);
+            Console.SetOut(result);
+
+            FurnitureManufacturerEngine.Instance.Start();
+
+            string expected = output.ReadToEnd().Trim();
+            string actual = result.ToString().Trim();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
